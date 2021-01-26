86505be7b3f5784b70d2a913b73f07682e002652
Qədir Hüseynov <qedirdev@gmail.com>
2021-01-26 14:16:49 +0300

    css update

--- js/app.js
+++ js/app.js
@@ -129,7 +129,7 @@ var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+      delay: 3000,
       disableOnInteraction: false,
     },
     pagination: {
