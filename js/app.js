35b9afb3c595ec8de6421f500828bc01e24cc883
Qədir Hüseynov <qedirdev@gmail.com>
2021-01-26 13:33:53 +0300

    update #3

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@
 
         // back to top
         $('.back-to-top').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 3000);
+            $("html, body").animate({ scrollTop: 0 }, 1500);
             return false;
         });
     }
@@ -122,3 +122,22 @@
     }); 
 
 }(jQuery)
+
+// Swiper Js
+
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
